2c4444976918fe88b97790c659ee149ab5e5fe3d
awest36 <awest36@rocketmail.com>
2017-10-22 21:40:35 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeImage(event) {
+    event = event || window.event;
+
+    var targetElement = event.target || event.srcElement;
+
+    if (targetElement.tagName == "IMG") {
+        document.getElementById("mainImage").src = targetElement.getAttribute("src");
+
+    }
+}
